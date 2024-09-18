a2f037ba9e8e15dc1d8b061cf44940e1a17df72c
SArRP0203 <bu11et.ctfofficial.1926@gmail.com>
2024-09-19 08:59:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const { exec } = require('child_process');
 
 app.use(express.static('public'));
 
-app.get('/cowsay/:message/:animal', (req, res) => {
+app.get('/cowsay/:animal/:message', (req, res) => {
   const animal = req.params.animal;
   const message = req.params.message;
-  exec(`/usr/games/cowsay -f ${animal} ${message}`, {timeout: 5000}, (error, stdout) => {
+  exec(`/usr/games/cowsay -f ${animal} ${message}`, { timeout: 5000 }, (error, stdout) => {
     if (error) return res.status(500).send("Error with cowsay command.");
     res.type('txt').send(stdout).end();
   });
