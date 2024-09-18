7f68899aa67997ce9673439dddd4d127c7a28bde
SArRP0203 <bu11et.ctfofficial.1926@gmail.com>
2024-09-18 15:31:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.get('/cowsay/:animal/:message', (req, res) => {
   const animal = req.params.animal;    // 動物の種類（カスタムキャラクター含む）を取得
   const message = req.params.message;  // メッセージを取得
-  exec(`/usr/games/cowsay -f ${animal} ${message}`, { timeout: 5000 }, (error, stdout) => {
+  exec(`/usr/games/cowsay -f ${animal} -f ${message}`, { timeout: 5000 }, (error, stdout) => {
     if (error) return res.status(500).send("Error with cowsay command.");
     res.type('txt').send(stdout).end();
   });
